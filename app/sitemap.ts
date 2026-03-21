3329bb6b13861db4ab2bc949ff62d443be357533
karz-engawa <65191521+karz-engawa@users.noreply.github.com>
2026-03-22 01:40:22 +0900

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.自動採譜.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.自動採譜.com/tool",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.自動採譜.com/articles",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.自動採譜.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.自動採譜.com/privacy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.自動採譜.com/contact",
+      lastModified: new Date(),
+    },
+  ]
+}
